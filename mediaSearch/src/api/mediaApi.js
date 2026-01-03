3b9b4b96a23675d7b30fb2365274cde5c0a7d624
Abhay Chauhan <chauhanabhay88688@gmail.com>
2026-01-03 18:22:19 +0530

    add fetch gif api

--- mediaSearch/src/api/mediaApi.js
+++ mediaSearch/src/api/mediaApi.js
@@ -2,6 +2,7 @@ import axios from "axios";
 
 const UNSPLASH_KEY = import.meta.env.VITE_UNSPLASH_API_KEY;
 const PEXELS_KEY = import.meta.env.VITE_PEXELS_API_KEY;
+const TENOR_KEY = import.meta.env.VITE_TENOR_API_KEY;
 
 export async function fetchPhotos(query, page = 1, per_page = 20) {
   const res = await axios.get("https://api.unsplash.com/search/photos", {
@@ -15,6 +16,14 @@ export async function fetchVideos(query, page = 1, per_page = 15) {
   const res = await axios.get("https://api.pexels.com/videos/search", {
     params: { query, page, per_page },
     headers: { Authorization: `Client-ID ${PEXELS_KEY}` },
+  });
+
+  return res.data;
+}
+
+export async function fetchGif(query, limit = 20) {
+  const res = await axios.get("https://tenor.googleapis.com/v2/search", {
+    params: { q: query, key: TENOR_KEY, limit },
   });
 
   return res.data;
