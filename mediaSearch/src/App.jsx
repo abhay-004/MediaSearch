430cc6c56220a323e64cc5bcb38acac595929eb1
Abhay Chauhan <chauhanabhay88688@gmail.com>
2026-01-03 17:57:35 +0530

    add fetchVideos api

--- mediaSearch/src/App.jsx
+++ mediaSearch/src/App.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { fetchPhotos } from "./api/mediaApi";
+import { fetchPhotos, fetchVideos } from "./api/mediaApi";
 
 const App = () => {
   //fetch images from unsplash api
@@ -7,10 +7,27 @@ const App = () => {
     const data = await fetchPhotos("cat");
     console.log(data.results);
   };
+
+  //fetch videos from pexels
+
+  const fetchClips = async () => {
+    const data = await fetchVideos("cat");
+    console.log(data.videos);
+  };
+
   return (
     <div className="h-screen w-full bg-gray-950  text-white ">
-      <button className="border px-4 py-2 cursor-pointer" onClick={fetchImages}>
+      <button
+        className="border bg-green-400 m-5 active:scale-95 px-4 py-2 cursor-pointer"
+        onClick={fetchImages}
+      >
         Get Photos
+      </button>
+      <button
+        className="border bg-green-400 m-5 active:scale-95 px-4 py-2 cursor-pointer"
+        onClick={fetchClips}
+      >
+        Get Videos
       </button>
     </div>
   );
--- mediaSearch/src/api/mediaApi.js
+++ mediaSearch/src/api/mediaApi.js
@@ -10,3 +10,12 @@ export async function fetchPhotos(query, page = 1, per_page = 20) {
   });
   return res.data;
 }
+
+export async function fetchVideos(query, page = 1, per_page = 15) {
+  const res = await axios.get("https://api.pexels.com/videos/search", {
+    params: { query, page, per_page },
+    headers: { Authorization: `Client-ID ${PEXELS_KEY}` },
+  });
+
+  return res.data;
+}
