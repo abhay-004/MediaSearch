2d57aadeeb81ba809d0a7292ef8ffb66c5bd48d8
Abhay Chauhan <chauhanabhay88688@gmail.com>
2026-01-04 16:27:43 +0530

    create tabs component

--- mediaSearch/src/App.jsx
+++ mediaSearch/src/App.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 import SearchBar from "./components/SearchBar";
+import Tabs from "./components/Tabs";
 
 const App = () => {
   return (
     <div className="h-screen w-full bg-gray-950  text-white ">
       <SearchBar />
+      <Tabs/>
     </div>
   );
 };
--- mediaSearch/src/components/Tabs.jsx
+++ mediaSearch/src/components/Tabs.jsx
@@ -1,11 +1,24 @@
-import React from 'react'
+import React from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { setActiveTabs } from "../redux/features/searchSlice";
 
 const Tabs = () => {
+  const tabs = ["photos", "videos", "GIF"];
+  const dispatch = useDispatch();
+  const activeTab = useSelector((state)=>state.search.activeTab)
   return (
-    <div>
-      
+    <div className="flex gap-5 p-10">
+      {tabs.map((ele, idx) => (
+        <button
+          className={`${activeTab==ele?"bg-blue-600":"bg-gray-500"} px-5 py-2 rounded uppercase cursor-pointer active:scale-95 "`}
+          key={idx}
+          onClick={() => dispatch(setActiveTabs(ele))}
+        >
+          {ele}
+        </button>
+      ))}
     </div>
-  )
-}
+  );
+};
 
-export default Tabs
+export default Tabs;
