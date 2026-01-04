614b92199503c9587cb6152b9624821f2c56a151
Abhay Chauhan <chauhanabhay88688@gmail.com>
2026-01-04 15:30:16 +0530

    create searchBar component

--- mediaSearch/src/App.jsx
+++ mediaSearch/src/App.jsx
@@ -1,47 +1,10 @@
 import React from "react";
-import { fetchGif, fetchPhotos, fetchVideos } from "./api/mediaApi";
+import SearchBar from "./components/SearchBar";
 
 const App = () => {
-  //fetch images from unsplash api
-  const fetchImages = async () => {
-    const data = await fetchPhotos("cat");
-    console.log(data.results);
-  };
-
-  //fetch videos from pexels
-
-  const fetchClips = async () => {
-    const data = await fetchVideos("cat");
-    console.log(data.videos);
-  };
-
-  //fetch gif from tenor
-
-  const fetchGIF = async () => {
-    const data = await fetchGif("cat");
-    console.log(data.results);
-  };
-
   return (
     <div className="h-screen w-full bg-gray-950  text-white ">
-      <button
-        className="border bg-green-400 m-5 active:scale-95 px-4 py-2 cursor-pointer"
-        onClick={fetchImages}
-      >
-        Get Photos
-      </button>
-      <button
-        className="border bg-green-400 m-5 active:scale-95 px-4 py-2 cursor-pointer"
-        onClick={fetchClips}
-      >
-        Get Videos
-      </button>
-      <button
-        className="border bg-green-400 m-5 active:scale-95 px-4 py-2 cursor-pointer"
-        onClick={fetchGIF}
-      >
-        Get Gif
-      </button>
+      <SearchBar />
     </div>
   );
 };
