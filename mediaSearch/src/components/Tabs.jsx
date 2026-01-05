1f8558b1ced80f94173ba393cbc2b9ac70a4002c
Abhay Chauhan <chauhanabhay88688@gmail.com>
2026-01-05 15:12:18 +0530

    fix array in tabs

--- mediaSearch/src/components/Tabs.jsx
+++ mediaSearch/src/components/Tabs.jsx
@@ -3,14 +3,16 @@ import { useDispatch, useSelector } from "react-redux";
 import { setActiveTabs } from "../redux/features/searchSlice";
 
 const Tabs = () => {
-  const tabs = ["photos", "videos", "GIF"];
+  const tabs = ["photos", "videos", "gif"];
   const dispatch = useDispatch();
-  const activeTab = useSelector((state)=>state.search.activeTab)
+  const activeTab = useSelector((state) => state.search.activeTab);
   return (
     <div className="flex gap-5 p-10">
       {tabs.map((ele, idx) => (
         <button
-          className={`${activeTab==ele?"bg-blue-600":"bg-gray-500"} px-5 py-2 rounded uppercase cursor-pointer active:scale-95 "`}
+          className={`${
+            activeTab == ele ? "bg-blue-600" : "bg-gray-500"
+          } px-5 py-2 rounded uppercase cursor-pointer active:scale-95 "`}
           key={idx}
           onClick={() => dispatch(setActiveTabs(ele))}
         >
