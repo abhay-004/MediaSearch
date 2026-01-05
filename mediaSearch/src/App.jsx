5c5e26d7016d2daeb801e2604b83705c1f024e82
Abhay Chauhan <chauhanabhay88688@gmail.com>
2026-01-05 15:12:39 +0530

    add result grid in app

--- mediaSearch/src/App.jsx
+++ mediaSearch/src/App.jsx
@@ -1,12 +1,14 @@
 import React from "react";
 import SearchBar from "./components/SearchBar";
 import Tabs from "./components/Tabs";
+import ResultGrid from "./components/ResultGrid";
 
 const App = () => {
   return (
     <div className="h-screen w-full bg-gray-950  text-white ">
       <SearchBar />
-      <Tabs/>
+      <Tabs />
+      <ResultGrid />
     </div>
   );
 };
