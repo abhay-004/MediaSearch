7be2d4e06fd98613de350da1ef3d88958ba7b423
Abhay Chauhan <chauhanabhay88688@gmail.com>
2026-01-07 15:01:20 +0530

    fix height

--- mediaSearch/src/App.jsx
+++ mediaSearch/src/App.jsx
@@ -5,7 +5,7 @@ import ResultGrid from "./components/ResultGrid";
 
 const App = () => {
   return (
-    <div className="h-screen w-full bg-gray-950  text-white ">
+    <div className="min-h-screen w-full bg-gray-950  text-white ">
       <SearchBar />
       <Tabs />
       <ResultGrid />
