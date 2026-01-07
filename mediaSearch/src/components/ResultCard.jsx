e34aa84991e222f6b1806a02adc6c6aa1ec7ed0d
Abhay Chauhan <chauhanabhay88688@gmail.com>
2026-01-07 15:02:07 +0530

    create result card

--- mediaSearch/src/components/ResultCard.jsx
+++ mediaSearch/src/components/ResultCard.jsx
@@ -1,11 +1,48 @@
-import React from 'react'
+import React from "react";
 
-const ResultCard = () => {
+const ResultCard = ({ item }) => {
   return (
-    <div>
-      
+    <div className="w-[18vw] relative h-70 bg-white rounded">
+      <a href={item.url} target="_blank" className="h-full">
+        {item.type == "photo" ? (
+          <img
+            className="h-full w-full object-cover object-center "
+            src={item.src}
+          />
+        ) : (
+          ""
+        )}
+        {item.type == "video" ? (
+          <video
+            className="h-full w-full object-cover object-center "
+            autoPlay
+            muted
+            loop
+            src={item.src}
+          ></video>
+        ) : (
+          ""
+        )}
+        {item.type == "gif" ? (
+          <img
+            className="h-full w-full object-cover object-center "
+            src={item.src}
+          />
+        ) : (
+          ""
+        )}
+      </a>
+      <div
+        id="bottom"
+        className="text-white flex justify-between items-center  w-full px-6 py-10 gap-2 absolute bottom-0"
+      >
+        <h2 className="text-lg font-semibold capitalize">{item.title}</h2>
+        <button className="bg-indigo-600 text-white rounded px-3 py-1 cursor-pointer font-medium">
+          Save
+        </button>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default ResultCard
+export default ResultCard;
